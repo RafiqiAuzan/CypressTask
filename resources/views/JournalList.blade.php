0680e02725afe7905ddab082c255ccebd9ddf80c
RafiqiAuzan <rafikids32@gmail.com>
2024-10-10 17:10:53 +0700

    style: reformat code for better readability

--- resources/views/JournalList.blade.php
+++ resources/views/JournalList.blade.php
@@ -19,78 +19,57 @@
     </style>
 </head>
 
-<body class="antialiased">
-    <div class="bg-[#2F3645] h-screen">
-        <section class="">
-            <div class="">
-                <div class="container mx-auto pt-16">
-                    <a
-                        class="block ml-[394px] w-10 text-center p-1 mb-2 text-white bg-[#667c87] hover:bg-[#495961] font-medium rounded-lg"
-                        href="/journal-add">
-                        <i class="fa-solid fa-plus"></i>
-                    </a>
-                    <div class="">
-                        <div class="mx-auto  w-[750px] relative overflow-x-auto shadow-md sm:rounded-lg">
-                            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                <thead
-                                    class="text-xs text-white uppercase bg-gray-700">
-                                    <tr>
-                                        <th scope="col" class="px-6 py-3">
-                                            Title
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Name
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Date Published
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            ACTION
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($journals as $journal)
-                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                            <th scope="row"
-                                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                {{ $journal->journal_title }}
-                                            </th>
-                                            <td class="px-6 py-4">
-                                                {{ $journal->journal_name }}
-                                            </td>
-                                            <td class="px-6 py-4">
-                                                {{ $journal->created_at }}
-                                            </td>
-                                            <td class="py-4 flex gap-4">
-                                                <a href="#" class=" bg-[#712c2c] text-white py-1 px-2 rounded-lg"
-                                                    onclick="
-                                                        event.preventDefault();
-                                                        if (confirm('Do you want to remove this?')) {
-                                                        document.getElementById('delete-row-{{ $journal->id }}').submit();
-                                                        }">
-                                                    <i class="fa-regular fa-trash-can"></i>
-                                                </a>
-                                                <form id="delete-row-{{ $journal->id }}"
-                                                    action="{{ route('journals.destroy', ['id' => $journal->id]) }}"
-                                                    method="POST">
-                                                    <input type="hidden" name="_method" value="DELETE">
-                                                    @csrf
-                                                </form>
-                                                <a href="#" class="bg-[#667c87] text-white py-1 px-2 rounded-lg">
-                                                    <i class="fa-solid fa-pen-to-square"></i>
-                                                </a>
-                                                
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+<body>
+    <div class="bg-[#2F3645] h-screen flex justify-center items-center">
+        <div class="">
+            {{-- Button add data --}}
+            <a
+                class="block w-10 text-center p-1 mb-2 text-white bg-[#667c87] hover:bg-[#495961] rounded-lg"
+                href="/journal-add">
+                <i class="fa-solid fa-plus"></i>
+            </a>
+            {{-- table --}}
+            <div class="w-full overflow-x-auto rounded-lg">
+                <table class="text-sm text-left px-10 text-gray-400">
+                    <thead class="text-xs text-white uppercase bg-gray-700">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">Title</th>
+                            <th scope="col" class="px-6">Name</th>
+                            <th scope="col" class="px-6">Date Published</th>
+                            <th scope="col" class="px-6 text-center">ACTION</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($journals as $journal)
+                            <tr class=" border-b bg-gray-800 border-gray-700">
+                                <th scope="row" class="px-6 py-3 text-white">{{ $journal->journal_title }}</th>
+                                <td class="px-6">{{ $journal->journal_name }}</td>
+                                <td class="px-6">{{ $journal->created_at }}</td>
+                                <td class="px-6 py-2 flex gap-2">
+                                    <a href="#" class=" bg-[#712c2c] text-white py-1 px-2 rounded-lg"
+                                        onclick="
+                                            event.preventDefault();
+                                            if (confirm('Do you want to remove this?')) {
+                                            document.getElementById('delete-row-{{ $journal->id }}').submit();
+                                            }">
+                                        <i class="fa-regular fa-trash-can"></i>
+                                    </a>
+                                    <form id="delete-row-{{ $journal->id }}"
+                                        action="{{ route('journals.destroy', ['id' => $journal->id]) }}"
+                                        method="POST">
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        @csrf
+                                    </form>
+                                    <a href="#" class="bg-[#667c87] text-white py-1 px-2 rounded-lg">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
-        </section>
+        </div>
     </div>
 </body>
 
